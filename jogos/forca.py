8578fde7752dfcc27029e56d07418d49bf6c65c7
Junior <junior.jaava@gmail.com>
2020-03-26 12:44:06 -0300

    + adding menu to chose which game to play

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,8 +1,9 @@
 
-print("*********************************")
-print("Bem vindo ao jogo de Forca!")
-print("*********************************")
+def jogar():
+    print("*********************************")
+    print("Bem vindo ao jogo de Forca!")
+    print("*********************************")
 
 
-print("Fim do jogo")
+    print("Fim do jogo")
 
